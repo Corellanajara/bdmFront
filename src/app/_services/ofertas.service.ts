6315461e3357d51e05437e42e2209ea7bacd7945
Your Name <you@example.com>
2019-08-29 19:44:20 -0400

    version usable phone

--- src/app/_services/ofertas.service.ts
+++ src/app/_services/ofertas.service.ts
@@ -5,21 +5,41 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class OfertasService {
-
+  private testing = false;
   private url: string = "http://157.230.233.162:4600";
-
+  private ofertas = [];
   constructor(private http: HttpClient) { }
 
-  listar() {
-    return this.http.get<any[]>(`${this.url}/ofertas/`, {
-      headers: new HttpHeaders().set('Content-Type', 'application/json')
-    });
-  }
   insertar(oferta : any){
     return this.http.post<any[]>(`${this.url}/ofertas/`, oferta ,{
       headers: new HttpHeaders().set('Content-Type', 'application/json')
     });
   }
+  listar() {
+    return this.http.get<any[]>(`${this.url}/ofertas/`, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    });
+  }
+/*
+  listar() {
+    if(this.testing){
+      return this.ofertas;
+    }else{
+      return this.http.get<any[]>(`${this.url}/ofertas/`, {
+        headers: new HttpHeaders().set('Content-Type', 'application/json')
+      });
+    }
+  }
+  insertar(oferta : any){
+    if(this.testing){
+      this.ofertas.push(oferta);
+    }else{
+      return this.http.post<any[]>(`${this.url}/ofertas/`, oferta ,{
+        headers: new HttpHeaders().set('Content-Type', 'application/json')
+      });
+    }
+  }
+  */
   actualizar(id:number,causa : any){
     return this.http.put<any[]>(`${this.url}/causas/${id}`, causa ,{
       headers: new HttpHeaders().set('Content-Type', 'application/json')
